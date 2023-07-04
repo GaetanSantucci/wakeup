70ed8d1affc9ae9aabff39d53c36ccda4ef10cf8
GaetanSantucci <gaetan.santucci@outlook.com>
2023-07-04 16:17:43 +0200

    :boom: refacto methods and css to scroll to top button

--- wakeup_app/src/components/ScrollUp/index.js
+++ wakeup_app/src/components/ScrollUp/index.js
@@ -3,7 +3,8 @@ import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
 import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
 import styles from './Scroll.module.scss';
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useScratch } from 'react-use';
 
 const ScrollUp = () => {
   useEffect(() => window.document.scrollingElement?.scrollTo({
@@ -22,8 +23,38 @@ const ScrollToTop = () => {
 }
 
 const ButtonToScrollTop = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [isButtonScroll, setIsButtonScroll] = useState(false)
+  console.log('isButtonScroll:', isButtonScroll);
+  // console.log('screenHeight:', screenHeight);
+  // console.log('scrollPosition:', scrollPosition);
+
+  const handleScroll = () => {
+    const userScreen = window.innerHeight;
+    const position = window.scrollY;
+    setScreenHeight(userScreen);
+    setScrollPosition(position);
+  };
+
+  const goToTop = () => {
+    window.document.scrollingElement?.scrollTo(0, 0)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.btn_scrollTop} onClick={() => window.document.scrollingElement?.scrollTo(0, 0)}><ArrowBackIosOutlinedIcon /><ArrowDropUpOutlinedIcon /></div>
+    <>
+      {
+        scrollPosition > screenHeight ? <div className={styles.btn_scrollTop} onClick={goToTop}><ArrowBackIosOutlinedIcon /><ArrowDropUpOutlinedIcon /></div> : null
+      }
+    </>
   )
 }
 
